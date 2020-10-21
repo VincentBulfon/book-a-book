04955c5a404ca73b043e7766937f6434528e72e4
vincent <vincent.bulfon@gmail.com>
2020-10-21 20:59:07 +0200

    Add migrations :sparkles:

---
+++ bab_app/database/migrations/2020_10_20_131903_create_books_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->string('title', 255);
+            $table->integer('textual_content_id')->unsigned()->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
