1da8137ecab8912bfce3c16a69c6936ad1cf18f9
vincent <vincent.bulfon@gmail.com>
2020-10-28 16:45:34 +0100

    :construction: Add general app menu and received orders page

---
+++ bab_app/resources/views/components/app-menu.blade.php
@@ -0,0 +1,12 @@
+<section>
+    <h2>Menu de l'application</h2>
+    <!-- TODO add elements to link -->
+    <a href=""><img
+            src=""
+            alt=""
+        >
+        <!-- TODO student name-->
+    </a>
+    <!-- TODO ajouter le lien de déconnection -->
+    <a href="">Se déconnecter</a>
+</section>
